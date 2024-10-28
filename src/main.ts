a12b1adf391f67ea9f75b5fe43d02181b03ad0dd
Helen Shatynska <helen.shatynska@gmail.com>
2024-10-28 11:09:29 +0200

    feat: add versioning & openApi document

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
+import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
+  const documentConfig = new DocumentBuilder()
+    .setTitle('CashBook')
+    .setDescription('Description of CashBook API')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, documentConfig);
+  SwaggerModule.setup('v1', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
